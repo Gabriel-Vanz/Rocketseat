183e3d7830554b367b99e72feff9ab0d74ba0133
Gabriel Vanz <gabriel.vanz.silva@gmail.com>
2023-12-13 18:38:41 -0300

    fix: routes file

--- Ignite/NodeJS/01-fundamentos-node/src/routes.js
+++ Ignite/NodeJS/01-fundamentos-node/src/routes.js
@@ -1,50 +1,65 @@
-import { Database } from './middlewares/database.js';
-import { randomUUID } from 'node:crypto';
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
-    {
-        method: 'GET',
-        path: '/users',
-        handler: (req, res) => {
-            const users = database.select('users')
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { search } = req.query
 
-            return res.end(JSON.stringify(users))
-        }
-    },
-    {
-        method: 'POST',
-        path: '/users',
-        handler: (req, res) => {
-            const {name, email} = req.body
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      } : null)
 
-            const user = {
-                id: randomUUID(), 
-                name, 
-                email
-            }
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { name, email } = req.body
 
-        database.insert('users', user)
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      }
 
-        return res.writeHead(201).end()
-        }
-    },
-    {
-        method: 'DELETE',
-        path: '/users',
-        handler: (req, res) => {
-            const {name, email} = req.body
+      database.insert('users', user)
 
-            const user = {
-                id: randomUUID(), 
-                name, 
-                email
-            }
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
 
-        database.insert('users', user)
+      database.update('users', id, {
+        name,
+        email,
+      })
 
-        return res.writeHead(201).end()
-        }
-    },
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  }
 ]
